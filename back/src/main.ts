0a54325a249e7f3fb5f9a4d83f236cce2c09a2f6
Emily Morales <15ruth05@gmail.com>
2024-08-10 16:57:11 -0300

    mas pruebas

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swaggerConfig';
 import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 const PORT = process.env.PORT || 3005;
 
@@ -22,6 +23,9 @@ async function bootstrap() {
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
   });
+  // Configura Socket.IO usando el IoAdapter de NestJS
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   await app.listen(PORT);
   console.log(`Server listening on http:localhost:${PORT}`);
 }
